87275092129c2cceb927ce66e5c155b1a49094b6
Chunchunmaruinho <axelprogaymer33d@gmail.com>
2023-08-08 10:23:28 -0300

    <chore>[]: <add array.js>

---
+++ array.js
@@ -0,0 +1,13 @@
+let productos1 = {
+    nombre : 'coc'
+    precio : 'free'
+  }
+  let producto2 = {
+    nombre: 'cr'
+    precio: 'free'
+  }
+  
+  let objetos = [producto1,productos2]
+  
+  objetos
+  
